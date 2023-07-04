7e20fa5f7ebff305ce2d8ef79007b241138268db
Youssef El Goumri <94170257+youssefelgoumri@users.noreply.github.com>
2023-07-05 00:35:00 +0100

    first commit

---
+++ SD_TP6/src/main/java/com/example/sd_tp6/SdTp6Application.java
@@ -0,0 +1,13 @@
+package com.example.sd_tp6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdTp6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdTp6Application.class, args);
+    }
+
+}
---
+++ SD_TP6/src/test/java/com/example/sd_tp6/SdTp6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sd_tp6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdTp6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
